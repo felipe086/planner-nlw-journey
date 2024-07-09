c243bea627e25271acd5f0a79ebf1d954734e508
felipe086 <felipegalegoosawa@gmail.com>
2024-07-09 00:01:00 -0300

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,15 @@ import {
   serializerCompiler,
   validatorCompiler,
 } from 'fastify-type-provider-zod'
+import fastifyCors from '@fastify/cors'
 import { createTrip } from './routes/CreateTrip'
 
 export const app = fastify()
+
+app.register(fastifyCors, {
+  origin: '*',
+  credentials: true,
+})
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
